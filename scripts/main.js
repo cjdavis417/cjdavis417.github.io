7445b4a9698a9b53ac201163c51e907feffed8bf
Christopher Davis <christopherjdavis417@gmail.com>
2018-02-11 11:06:18 -0500

    nav edits and added resume

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true,
+    onOpen: function(el) { },
+    onClose: function(el) { },
+});
