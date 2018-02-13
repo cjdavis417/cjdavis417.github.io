2a016265b82e1d839455fc3f0c4a0d3bcb039ae3
Christopher Davis <christopherjdavis417@gmail.com>
2018-02-13 16:45:52 -0500

    fixing experience and trying to get the navbar to work.

--- scripts/helper.js
+++ scripts/helper.js
@@ -7,5 +7,54 @@ var age = Math.abs(ageStamp.getUTCFullYear() - 1970)
 document.getElementById('age').innerHTML = age;
 
 
+/******* navigation stuff **********/
+$(document).ready(function()
+{
+    applyNavigation();
+
+});
+
+// navigation functions
+var lnStickyNavigation;
+
+function applyNavigation() {
+    applyScrollSpy();
+    applyStickyNavigation();
+    navHighlight();
+}
+
+function applyScrollSpy() {
+    $('#navbar-example').on('activate.bs.scrollspy', function() {
+        window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+    });
+}
+
+function applyStickyNavigation() {
+    lnStickyNavigation = $('.scroll-down').offset().top + 20;
+    $(window).on('scroll', function() {
+        stickyNavigation();
+    });
+    stickyNavigation();
+}
+
+function stickyNavigation() {
+    if($(window).scrollTop() > lnStickyNavigation) {
+        $('body').addClass('fixed');
+    } else {
+        $('body').removeClass('fixed');
+    }
+}
+
+function navHighlight() {
+
+    var loc = $('#experiences').offset().top;
+
+    if (loc < 0) {
+        console.log('experience reached the top' + loc);
+        $('li').addClass('stickyNavActive');
+    }
+}
+
+
 
 
