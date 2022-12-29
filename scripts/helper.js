432d02949fe233124cf0c61f2498af154bfe2260
Chris Davis <christopherjdavis417@gmail.com>
2022-12-29 17:30:14 -0500

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -67,7 +67,7 @@ function wayPoints(hash) {
 }
 
 // json technical abilities
-
+console.log("testing 1");
 fetch(abilities.json)
 .then(response => response.json())
 .then(json => console.log(json));
