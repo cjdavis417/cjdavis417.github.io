c8dc4e7c296bb683902b241cad775783874f6123
Chris Davis <christopherjdavis417@gmail.com>
2022-12-29 17:37:17 -0500

    Update helper2.js

--- scripts/helper2.js
+++ scripts/helper2.js
@@ -1,5 +1,9 @@
 // json technical abilities
 console.log("testing 1");
+
+const abilityid = document.getElementById("abilities");
+abilityid.innerHTML = "Testing 123";
+
 fetch(abilities.json)
 .then(response => response.json())
 .then(json => console.log(json));
